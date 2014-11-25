8cec51746bc5d0926420b0fd51cd44091b74a32d
liushulong <hvk687@gmail.com>
2014-11-25 14:36:25 +0800

    change project layout

---
+++ src/com/dream/app/quicklaunch/MainActivity.java
@@ -0,0 +1,90 @@
+package com.dream.app.quicklaunch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import com.mobeta.android.dslv.DragSortController;
+import com.mobeta.android.dslv.DragSortListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    ArrayAdapter<String> adapter;
+
+    private String[] array;
+    private ArrayList<String> list;
+
+    private DragSortListView.DropListener onDrop =
+            new DragSortListView.DropListener() {
+                @Override
+                public void drop(int from, int to) {
+                    if (from != to) {
+                        String item = adapter.getItem(from);
+                        adapter.remove(item);
+                        adapter.insert(item, to);
+                    }
+                }
+            };
+
+    private DragSortListView.RemoveListener onRemove =
+            new DragSortListView.RemoveListener() {
+                @Override
+                public void remove(int which) {
+                    adapter.remove(adapter.getItem(which));
+                }
+            };
+
+    /**
+     * Return list item layout resource passed to the ArrayAdapter.
+     */
+    protected int getItemLayout() {
+        return R.layout.list_item_handle_left;
+    }
+
+    private DragSortListView mDslv;
+    private DragSortController mController;
+
+    public int dragStartMode = DragSortController.ON_DOWN;
+    public boolean removeEnabled = false;
+    public int removeMode = DragSortController.FLING_REMOVE;
+    public boolean sortEnabled = true;
+    public boolean dragEnabled = true;
+
+    public DragSortController getController() {
+        return mController;
+    }
+
+    /**
+     * Called in onCreateView. Override this to provide a custom
+     * DragSortController.
+     */
+    public DragSortController buildController(DragSortListView dslv) {
+        // defaults are
+        //   dragStartMode = onDown
+        //   removeMode = flingRight
+        DragSortController controller = new DragSortController(dslv);
+        controller.setDragHandleId(R.id.drag_handle);
+        controller.setClickRemoveId(R.id.click_remove);
+        controller.setRemoveEnabled(removeEnabled);
+        controller.setSortEnabled(sortEnabled);
+        controller.setDragInitMode(dragStartMode);
+        controller.setRemoveMode(removeMode);
+        return controller;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mDslv = (DragSortListView) findViewById(android.R.id.list);
+
+        mController = buildController(mDslv);
+        mDslv.setFloatViewManager(mController);
+        mDslv.setOnTouchListener(mController);
+        mDslv.setDragEnabled(dragEnabled);
+        mDslv.setDropListener(onDrop);
+        mDslv.setRemoveListener(onRemove);
+        //setListAdapter();
+    }
+}
